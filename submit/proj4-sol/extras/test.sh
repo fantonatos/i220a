a5ec9e35d7d15c63af672c6591626ff194c63bbc
Fotis Antonatos <fantona1@cs.binghamton.edu>
2020-11-22 23:07:40 -0500

    inintial progress on project4

---
+++ submit/proj4-sol/extras/test.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#assumes simulator in current directory
+
+TMPDIR=$HOME/tmp
+mkdir -p $TMPDIR
+
+PRG=./y86-sim
+
+for f in "$@"
+do
+    gold=`echo $f | sed -e 's/\.ys$/.out/'`
+
+    if [ -e $gold ]
+    then
+    	tmp=$TMPDIR/$(basename $gold)
+	if echo $f | grep -q 'main' 
+	then
+	    $PRG $f `seq 1 10` > $tmp
+	else
+	    $PRG $f > $tmp
+	fi
+    	if [ $? -eq 0 ]
+    	then
+    	    if diff $gold $tmp
+    	    then
+    		rm -f $tmp
+    	    else
+    		echo "*** $f failed; see output in $tmp"
+    	    fi
+    	else
+    	    echo "cannot run \"$PRG $f > $tmp\""
+    	fi
+    else
+    	echo "no out file for $f"
+    fi
+done
---
+++ submit/proj4-sol/ysim.c
@@ -0,0 +1,229 @@
+
+
+#include "ysim.h"
+#include <stdio.h>
+#include "errors.h"
+
+typedef enum {
+  HALT_CODE, NOP_CODE, CMOVxx_CODE, IRMOVQ_CODE, RMMOVQ_CODE, MRMOVQ_CODE,
+  OP1_CODE, Jxx_CODE, CALL_CODE, RET_CODE,
+  PUSHQ_CODE, POPQ_CODE } BaseOpCode;
+
+/************************** Utility Routines ****************************/
+
+/** Return nybble from op (pos 0: least-significant; pos 1:
+ *  most-significant)
+ */
+static Byte
+get_nybble(Byte op, int pos) {
+  return (op >> (pos * 4)) & 0xF;
+}
+
+
+static Word
+get_argument(Word instruction)
+{
+  Byte opcode = get_nybble(instruction, 1), offset = 0;
+  
+  switch(opcode)
+  {
+    case CALL_CODE:
+    case Jxx_CODE:
+      offset = 1;
+      break;
+    case IRMOVQ_CODE:
+    case RMMOVQ_CODE:
+    case MRMOVQ_CODE:
+      offset = 2;
+      break;
+  }
+  
+  Word argument = 0x00000000;
+  for (int byte = offset + 4; byte > offset; byte--)
+  {
+    argument |= get_nybble(instruction, byte) << byte * 4;
+  }
+  
+  return argument;
+}
+/************************** Condition Codes ****************************/
+
+/** Conditions used in instructions */
+typedef enum {
+  ALWAYS_COND, LE_COND, LT_COND, EQ_COND, NE_COND, GE_COND, GT_COND
+} Condition;
+
+/** accessing condition code flags */
+static inline bool get_cc_flag(Byte cc, unsigned flagBitIndex) {
+  return !!(cc & (1 << flagBitIndex));
+}
+static inline bool get_zf(Byte cc) { return get_cc_flag(cc, ZF_CC); }
+static inline bool get_sf(Byte cc) { return get_cc_flag(cc, SF_CC); }
+static inline bool get_of(Byte cc) { return get_cc_flag(cc, OF_CC); }
+
+/** Return true iff the condition specified in the least-significant
+ *  nybble of op holds in y86.  Encoding of Figure 3.15 of Bryant's
+ *  CompSys3e.
+ */
+bool
+check_cc(const Y86 *y86, Byte op)
+{
+  bool ret = false;
+  Condition condition = get_nybble(op, 0);
+  Byte cc = read_cc_y86(y86);
+  switch (condition) {
+    case ALWAYS_COND:
+      ret = true;
+      break;
+    case LE_COND:
+      ret = (get_sf(cc) ^ get_of(cc)) | get_zf(cc);
+      break;
+    //@TODO add other cases
+    default: {
+      Address pc = read_pc_y86(y86);
+      fatal("%08lx: bad condition code %d\n", pc, condition);
+      break;
+      }
+  }
+  return ret;
+}
+
+/** return true iff word has its sign bit set */
+static inline bool
+isLt0(Word word) {
+  return (word & (1UL << (sizeof(Word)*CHAR_BIT - 1))) != 0;
+}
+
+/** Set condition codes for addition operation with operands opA, opB
+ *  and result with result == opA + opB.
+ */
+static void
+set_add_arith_cc(Y86 *y86, Word opA, Word opB, Word result)
+{
+  //@TODO
+}
+
+/** Set condition codes for subtraction operation with operands opA, opB
+ *  and result with result == opA - opB.
+ */
+static void
+set_sub_arith_cc(Y86 *y86, Word opA, Word opB, Word result)
+{
+  //@TODO
+}
+
+static void
+set_logic_op_cc(Y86 *y86, Word result)
+{
+  //@TODO
+}
+
+/**************************** Operations *******************************/
+// Determine which mathematical function we are doing
+static void
+op1(Y86 *y86, Byte op, Register regA, Register regB)
+{
+  enum {ADDL_FN, SUBL_FN, ANDL_FN, XORL_FN };
+  
+  
+  
+  // Tasks required: Determine which function (Add, subtract, AND, XOR)
+  // Nibble 2 of instruction contains this
+  Byte function = get_nybble(op, 2);
+  switch(function)
+  {
+    case ADDL_FN:
+      break;
+    case SUBL_FN:
+      break;
+    case ANDL_FN:
+      break;
+    case XORL_FN:
+      break;
+    default:
+      break;
+  }
+  
+  //@TODO
+}
+
+/*********************** Single Instruction Step ***********************/
+
+/** Execute the next instruction of y86. Must change status of
+ *  y86 to STATUS_HLT on halt, STATUS_ADR or STATUS_INS on
+ *  bad address or instruction.
+ */
+void
+step_ysim(Y86 *y86)
+{
+  //printf("start \n");
+  
+  // Get this step's instuction, opcode, and increment program counter
+  Address counter = read_pc_y86(y86);
+  Word instruction = read_memory_word_y86(y86, counter);
+  Byte opcode = get_nybble((Byte)instruction, 1);
+  
+  
+  //printf("opcode: 0x%X\n", opcode);
+ 
+  /*
+   * Is there a situation? If so, determine which situation.
+   * If there is a situation, handle the situation
+   * in a manner apropriate for that situation.
+   * If there was not a situation, move on to the next situation.
+   */
+  Word addr = 0, data = 0;
+  Address dest = 0;
+  Register a = 0, b = 0;
+  switch(opcode)
+  {
+    case HALT_CODE:
+      write_status_y86(y86, STATUS_HLT);
+      return;
+    case NOP_CODE:
+      // Do nothing
+      break;
+    case CALL_CODE:
+      addr = read_register_y86(y86, REG_RSP);             // Get Stack Pointer
+      write_memory_word_y86(y86, addr, (Word)counter+1);  // Write return address to stack
+      write_register_y86(y86, REG_RSP, (Word)addr+1);     // Increment stack pointer
+      dest = get_argument(instruction);
+      write_pc_y86(y86, dest);                            // Set Program Counter to destination (jump)
+      return;
+    case RET_CODE:
+      addr = read_register_y86(y86, REG_RSP);             // Get Stack Pointer
+      write_register_y86(y86, REG_RSP, (Word)addr-1);     // Decrement stack pointer
+      dest = read_memory_word_y86(y86, addr-1);           // Read return address from stack
+      write_pc_y86(y86, dest);                            // Set Program Counter to returning destination (jump)
+      break;
+    case POPQ_CODE:
+      a = get_nybble(instruction, 3);                     // Get Destination Register
+      addr = read_register_y86(y86, REG_RSP);             // Get Stack Pointer
+      data = read_memory_word_y86(y86, addr);             // Read data from stack
+      write_register_y86(y86, REG_RSP, (Word)addr-1);     // Decrement stack pointer
+      write_register_y86(y86, a, data);                   // Write data to destination register
+      break;
+    case PUSHQ_CODE:
+      a = get_nybble(instruction, 3);                     // Get Source Register
+      addr = read_register_y86(y86, REG_RSP);             // Get Stack Pointer
+      write_register_y86(y86, REG_RSP, (Word)addr+1);     // Increment stack pointer
+      data = read_register_y86(y86, REG_RSP);             // Read data from source register
+      write_memory_word_y86(y86, addr+1, data);           // Write data to stack
+      break;
+    case OP1_CODE:
+      op1(y86, instruction,
+        get_nybble(instruction, 3),
+        get_nybble(instruction, 4));
+      break;
+    default:
+      // TODO Change CPU Status on Unrecognized Instruction
+      break;
+  }
+  //printf("conclude \n");
+  
+  // Change Status to OK, Increment Program Counter
+  write_status_y86(y86, STATUS_AOK);
+  write_pc_y86(y86, counter+1);
+  //printf("Complete Instruction opcode: 0x%X, counter was %d, now is %d\n", opcode, counter, counter+1);
+}
+
